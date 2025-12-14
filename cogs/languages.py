b6bbe8b41cbce9dcccb5d592167fa2cd06a63b70
ItsNeil17 <neil@willofsteel.me>
2025-12-14 19:48:55 +0530

    feat: add private threads for translators
    Signed-off-by: ItsNeil17 <neil@willofsteel.me>

---
+++ cogs/languages.py
@@ -0,0 +1,87 @@
+import discord
+from discord import app_commands
+from discord.ext import commands
+
+import logging
+
+log = logging.getLogger(__name__)
+
+TRANSLATOR_CHANNEL = 1446878843250938019
+LANGUAGES = ["Afrikaans", "Arabic", "Dutch", "French", "German", "Hindi", "Indonesian", "Italian", "Japanese", "Latvian", "Portuguese, Brazilian", "Romanian", "Russian", "Spanish", "Turkish", "Vietnamese"]
+# day 1 of writing bad code with neil - i'm too lazy i'm sorry, but it works for now. will revamp all this later.
+PROOF_READERS = {
+    "Russian": [754700328359821333, 415132870252822539],
+    "Portuguese, Brazilian": [174997562938490881],
+    "German": [643536207623618589]
+}
+
+class Languages(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot: commands.Bot = bot
+        self.db = bot.database
+        self.translation_channel: discord.TextChannel = self.bot.get_channel(TRANSLATOR_CHANNEL)
+
+        self.mention_translators_menu = app_commands.ContextMenu(
+            name="Mention Translators",
+            callback=self.mention_translators
+        )
+        self.bot.tree.add_command(self.mention_translators_menu)
+
+    @app_commands.command()
+    @app_commands.describe(language="Select the language you translate to")
+    @app_commands.choices(language=[app_commands.Choice(name=lang, value=lang) for lang in LANGUAGES])
+    async def translator(self, interaction: discord.Interaction, language: str):
+        thread: discord.Thread = None
+        for t in self.translation_channel.threads: # for people looking at this, I know it's not the best way but it works dynamically and I don't have to hard code IDs.
+            if t.name == f"{language} Discussion":
+                thread = t
+                break
+
+        if thread is None:
+            thread = await self.translation_channel.create_thread(name=f"{language} Discussion", type=discord.ChannelType.private_thread, auto_archive_duration=None, reason=f"Translator thread for {language} created by {interaction.user}.", invitable=False)
+            thread.add_user(self.bot.fetch_user(702385226407608341)) # Add neil to the thread
+            thread.add_user(self.bot.fetch_user(614523861429387439)) # Add okuna to the thread
+            log.info(f"Created new translator thread for {language}")
+
+        thread.add_user(interaction.user)
+        await interaction.response.send_message(f"You have been added to the translator thread for {language}.", ephemeral=True)
+
+    async def mention_translators(self, interaction: discord.Interaction, message: discord.Message) -> None:
+        if not isinstance(message.channel, discord.Thread):
+            await interaction.response.send_message(
+                "This message is not in a translator thread.", ephemeral=True
+            )
+            return
+        
+        channel: discord.Thread = interaction.channel
+        if channel.parent_id != 1446878843250938019:
+            await interaction.response.send_message(
+                "This message is not in a translator thread.", ephemeral=True
+            )
+            return
+        
+        language = channel.name.replace(" Discussion", "")
+        proofreaders = PROOF_READERS.get(language, [])
+        if interaction.user.id not in proofreaders:
+            await interaction.response.send_message(
+                "You are not authorized to mention translators for this language.", ephemeral=True
+            )
+            return
+
+        mentions = []
+        async for member in channel.fetch_members():
+            if member.id != interaction.user.id:
+                mentions.append(member.mention)
+
+        if not mentions:
+            await interaction.response.send_message(
+                "There are no other translators in this thread.", ephemeral=True
+            )
+            return
+
+        await interaction.response.send_message(
+            f"Mentioning translators for {language}: \n{', '.join(mentions)}"
+        )
+
+async def setup(bot):
+    await bot.add_cog(Languages(bot))
